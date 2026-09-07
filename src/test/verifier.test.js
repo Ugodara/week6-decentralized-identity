d0c98f15acebeac3c5baa43a00ca5494c8990ddd
ugodara <aiworkskill@gmail.com>
2026-09-07 12:18:22 +0100

    correct evidence and test folder

--- src/test/verifier.test.js
+++ src/test/verifier.test.js
@@ -11,7 +11,7 @@ import assert from 'node:assert/strict';
 import * as ed25519 from '@noble/ed25519';
 import { sha512 } from '@noble/hashes/sha2.js';
 import { base58btc } from 'multiformats/bases/base58';
-import { verifyCredential, ReasonCode } from '../src/verifier.js';
+import { verifyCredential, ReasonCode } from '../verifier.js';
 
 ed25519.hashes.sha512 = sha512;
 
